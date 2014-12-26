ca18b2a57e4432812ce3ccc2567d7a3c221e4927
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-12-26 15:10:30 +0700

    feat(bemusepack): convert ogg and wav into mp3

--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import program    from 'commander'
 import co         from 'co'
 import fs         from 'fs'
 import promisify  from 'es6-promisify'
+import merge      from 'merge-stream'
+import map        from 'map-stream'
+import endpoint   from 'endpoint'
+
+import { join, basename } from 'path'
+import { spawn } from 'child_process'
 
 program
 .version(require('../package.json').version)
@@ -24,11 +30,53 @@ if (program.args.length === 1) {
 
 function packIntoBemuse(dir) {
   return co(function*() {
+
     let stat = yield promisify(fs.stat)(dir)
-    if (!stat.isDirectory()) {
-      throw new Error('Not a directory: ' + dir)
-    }
-    console.log(stat)
+    if (!stat.isDirectory()) throw new Error('Not a directory: ' + dir)
+
+    let path = (...components) => join(dir, ...components)
+
+    let samples = merge(
+      
+      // MP3
+      gulp.src(path('*.mp3'), { stream: true, nocase: true }),
+
+      // WAV, OGG
+      gulp.src(path('*.{wav,ogg}'), { read: false, nocase: true })
+          .pipe(convertAudio())
+
+    )
+
+    let stream = merge(samples)
+    yield waitStream(stream)
+
+  })
+}
+
+function waitStream(stream) {
+  return new Promise((resolve, reject) => {
+    stream.on('end', resolve)
+    stream.on('error', reject)
+  })
+}
+
+function convertAudio(stream) {
+  return map(function(file, callback) {
+    let path = file.path
+    file.path = file.path.replace(/\.\w+$/, '.mp3')
+    let log = gutil.log.bind(gutil, '[convertAudio]',
+                basename(path), '->', basename(file.path))
+    let sox = spawn('sox', [path, '-t', 'mp3', '-'])
+    sox.stdin.end()
+    sox.stderr.on('data', x => process.stderr.write(x))
+    sox.stdout.pipe(endpoint(function(err, buffer) {
+      if (err) {
+        log('ERROR!')
+        return callback(err)
+      }
+      log('[' + Math.round(buffer.length / 1024) + 'kB]')
+      callback(null, file)
+    }))
   })
 }
 
