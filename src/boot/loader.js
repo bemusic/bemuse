3a62a60628a11f99e354dd56a181959c6b06e04d
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-12-28 23:06:42 +0700

    chore(boot): extract code splitting generator to boot/loader
    This helps us be less verbose when loading modules dynamically.  Instead of
    having to write things like this for each code-splitting point:
    
        require.ensure(["../app"], function(require) {
          finishLoading(require("../app"))
        })
    
    We can now write it like this:
    
        loadModule['app'](finishLoading)
    
    Closes #40

---
+++ src/boot/loader.js
@@ -0,0 +1,24 @@
+
+// This file holds a logic to generate a code-splitting loader function.
+// The code is compiled on build-time.
+
+var modules = {
+      app: '../app',
+      test: '../test',
+    }
+
+var code = 'module.exports = {'
+
+code += Object.keys(modules).map(function(key) {
+  var path = modules[key]
+  return JSON.stringify(key) + ': function(callback) {' +
+    'require.ensure(' + JSON.stringify([path]) + ', function(require) {' +
+      'callback(require(' + JSON.stringify(path) + '))' +
+    '})' +
+  '}'
+}).join(',\n')
+
+code += '}'
+
+module.exports = code
+
