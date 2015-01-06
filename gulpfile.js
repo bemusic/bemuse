5a59369ec059e86d974b46c5547d35845778d7e9
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-06 14:09:06 +0700

    chore: add code coverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,31 @@
 
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
+var istanbul = require('gulp-istanbul')
+
+var files = {
+  specs: ['spec/**/*_spec.js'],
+  sources: ['spec/**/*_sources.js'],
+}
+
+function test() {
+  global.expect = require('chai').expect
+  return gulp.src(files.specs, { read: false })
+    .pipe(mocha({reporter: 'nyan'}))
+}
 
 gulp.task('test', function() {
-  global.expect = require('chai').expect
-  return gulp.src('spec/**/*_spec.js', { read: false })
-    .pipe(mocha({reporter: 'nyan'}))
+  return test()
 })
 
+gulp.task('test-cov', function(callback) {
+  global.expect = require('chai').expect
+  gulp.src(files.sources)
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function() {
+      test()
+        .pipe(istanbul.writeReports())
+        .on('end', callback)
+    })
+})
