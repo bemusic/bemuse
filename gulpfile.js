503954682a3d92b2c07e9876cf0b95208ac30613
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-07 11:02:13 +0700

    refactor(gulpfile): put them into cover

--- gulpfile.js
+++ gulpfile.js
@@ -8,26 +8,40 @@ var files = {
   sources: ['spec/**/*_sources.js'],
 }
 
-function mochaTest() {
+function cover(fn, callback) {
+  if (process.env.COV === 'true') {
+    gulp.src(files.sources)
+      .pipe(istanbul())
+      .pipe(istanbul.hookRequire())
+      .on('finish', function() {
+        fn(function(error) {
+          if (error) return callback(error)
+          istanbul.writeReports()
+            .once('end', callback)
+            .once('error', callback)
+            .emit('end')
+        })
+      })
+  } else {
+    fn(callback)
+  }
+}
+
+function mochaTest(callback) {
   global.expect = require('chai').expect
   return gulp.src(files.specs, { read: false })
     .pipe(mocha({reporter: 'nyan'}))
+    .on('end', callback)
+    .on('error', callback)
 }
 
-gulp.task('test', ['test:mocha:cov'])
+gulp.task('test', ['test:mocha', 'test:cucumber'])
 
-gulp.task('test:mocha', function() {
-  return mochaTest()
+gulp.task('test:cucumber', function() {
+  console.log('TODO: Cucumber Test')
 })
 
-gulp.task('test:mocha:cov', function(callback) {
-  global.expect = require('chai').expect
-  gulp.src(files.sources)
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire())
-    .on('finish', function() {
-      mochaTest()
-        .pipe(istanbul.writeReports())
-        .on('end', callback)
-    })
+gulp.task('test:mocha', function(callback) {
+  return cover(mochaTest, callback)
 })
+
