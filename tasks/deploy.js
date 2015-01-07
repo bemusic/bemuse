1e9929767e092486082ee7693ab84130b3240956
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-07 11:49:07 +0700

    chore(tasks/deploy): deploy to GitHub pages
    Closes #58

---
+++ tasks/deploy.js
@@ -0,0 +1,12 @@
+
+import gulp   from 'gulp'
+import deploy from 'gulp-gh-pages'
+import path   from 'path'
+
+gulp.task('deploy', function () {
+  return gulp.src('dist/**/*')
+    .pipe(deploy({
+      cacheDir: path.resolve(__dirname, '../../www'),
+      branch: 'master',
+    }))
+})
