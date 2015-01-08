be683d0a71ab6481048caf9f985763223854c1b4
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-08 21:29:56 +0700

    feat(chart): replace overlapping objects

--- bms/chart.js
+++ bms/chart.js
@@ -29,6 +29,17 @@ function BMSObjects() {
 }
 
 BMSObjects.prototype.add = function(object) {
+  if (object.channel !== '01') {
+    for (var i = 0; i < this._objects.length; i ++) {
+      var test = this._objects[i]
+      if (test.channel === object.channel &&
+          test.measure === object.measure &&
+          test.fraction === object.fraction) {
+        this._objects[i] = object
+        return
+      }
+    }
+  }
   this._objects.push(object)
 }
 
