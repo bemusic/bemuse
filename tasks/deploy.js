44c8e5cef12decc15bab6e39c15d31cff9e9f43c
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-16 00:39:17 +0700

    chore(tasks/deploy): remove cacheDir

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -1,12 +1,10 @@
 
 import gulp   from 'gulp'
 import deploy from 'gulp-gh-pages'
-import path   from 'path'
 
 gulp.task('deploy', function () {
   return gulp.src('dist/**/*')
     .pipe(deploy({
-      cacheDir: path.resolve(__dirname, '../../www'),
       origin: 'www',
       branch: 'master',
     }))
