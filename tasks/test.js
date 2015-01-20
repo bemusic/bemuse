200e7429dfeff1e3600ff3d53eb6755d4d797e5c
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-01-20 17:14:23 +0700

    chore(tasks/test): add test:exit task to exit after testing
    Because browser-launcher keeps the browser around.

--- tasks/test.js
+++ tasks/test.js
@@ -6,3 +6,9 @@ gulp.task('test', function() {
   return run()
 })
 
+gulp.task('test:exit', function() {
+  return run().then(
+    function() { process.exit(0) },
+    function() { process.exit(1) }
+  )
+})
