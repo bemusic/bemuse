0d3ca6f37e144d091813992b8bf44606c02fc5f9
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-02-02 22:12:11 +0700

    chore(boot/error-dialog): skip coverage (we don't want to show error dialog)

--- src/boot/error-dialog.js
+++ src/boot/error-dialog.js
@@ -2,9 +2,12 @@
 import './error-dialog.scss'
 import template from './error-dialog.jade'
 
-export function show(message, url, line, col, e) {
+/* istanbul ignore next */
+function show(message, url, line, col, e) {
   var div = document.createElement('div')
   div.className = 'error-dialog'
   div.innerHTML = template({ message, url, line, col, e })
   document.body.appendChild(div)
 }
+
+export { show }
