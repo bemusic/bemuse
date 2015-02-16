c1e67787afa1f0449791d81194cea0d742790afc
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-02-16 22:27:34 +0700

    feat(loading-scene): perform data-binding using ractive

--- src/game/loading-scene/index.js
+++ src/game/loading-scene/index.js
@@ -4,6 +4,39 @@ import './style.scss'
 
 export default function LoadingScene() {
   return function(container) {
-    new View({ el: container })
+    let data = {
+      song: {
+        title: 'BY☆MY☆SIDE',
+        artist: 'iaht',
+        genre: 'Trance Core',
+      },
+      items: [
+        {
+          text: 'Loading game engine',
+          width: '20%',
+        },
+        {
+          text: 'Loading skin',
+          width: '50%',
+        },
+        {
+          text: 'Loading packages (3/5)',
+          width: '60%',
+        },
+        {
+          text: 'Loading sounds (853/1293)',
+          width: '66%',
+        },
+        {
+          text: 'Decoded KICK.ogg',
+          width: '32%',
+        },
+        {
+          text: 'Loading BGA (0/1)',
+          width: '0%',
+        },
+      ],
+    }
+    new View({ el: container, data })
   }
 }
