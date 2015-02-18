6fc330d126be0911bf4b74185f29c05234e80b4f
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-02-19 00:07:24 +0700

    chore(webpack): use absolute path in loader test

--- config/webpack.js
+++ config/webpack.js
@@ -35,11 +35,8 @@ let config = {
   module: {
     loaders: [
       {
-        test: /[\\\/]src[\\\/].*\.js$/,
-        loader: '6to5?modules=common&experimental=true',
-      },
-      {
-        test: /[\\\/]spec[\\\/].*\.js$/,
+        test: /\.js$/,
+        include: [path('src'), path('spec')],
         loader: '6to5?modules=common&experimental=true',
       },
       {
@@ -77,8 +74,13 @@ let config = {
 
 if (NODE_ENV === 'test' || process.env.COV === 'true') {
   config.module.postLoaders.push({
-    test: /\/src\/.*\.js$/,
-    exclude: /\/src\/test\/.*\.js$|\/src\/boot\/loader\.js$/,
+    test: /\.js$/,
+    include: [path('src')],
+    exclude: [
+      path('src', 'test'),
+      path('src', 'polyfill'),
+      path('src', 'boot', 'loader.js'),
+    ],
     loader: 'istanbul-instrumenter',
   })
 }
