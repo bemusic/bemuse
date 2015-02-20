574c6460ca697d37fb81e4151529f3ce5426a696
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-02-20 23:14:50 +0700

    test(notes): add more tests about long notes

--- spec/notes_spec.js
+++ spec/notes_spec.js
@@ -4,13 +4,19 @@ var Notes     = require('../notes')
 
 describe('Notes', function() {
 
-  it('should be able to parse normal notes', function() {
+  it('should be able to process normal notes', function() {
     var chart = Compiler.compile('#00111:01020300').chart
     var notes = Notes.fromBMSChart(chart)
     expect(notes.all()).to.have.length(3)
   })
 
-  it('should be able to parse autokeysounds', function() {
+  it('should be able to process long notes', function() {
+    var chart = Compiler.compile('#00151:01010202').chart
+    var notes = Notes.fromBMSChart(chart)
+    expect(notes.all()).to.have.length(2)
+  })
+
+  it('should be able to process autokeysounds', function() {
     var chart = Compiler.compile('#00101:01020300').chart
     var notes = Notes.fromBMSChart(chart)
     expect(notes.all()).to.have.length(3)
