1823166e67f8926ffe3b67eca985e514ac2489e6
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-03-03 15:21:39 +0700

    feat(WaveFactory): implement method stubs

--- src/game/audio/wave-factory.js
+++ src/game/audio/wave-factory.js
@@ -1,5 +1,27 @@
 
 export class WaveFactory {
+
+  constructor(master, samples) {
+    void master
+    void samples
+  }
+
+  // Plays an autokeysound note (using limited polyphony)
+  playAuto(keysound) {
+    void keysound
+  }
+
+  // Plays a hit note (using limited polyphony)
+  // Returns the SoundInstance which may be stopped when hold note is missed
+  playNote(keysound) {
+    void keysound
+  }
+
+  // Plays a note when hitting in the blank area (unlimited polyphony)
+  playFree(keysound) {
+    void keysound
+  }
+
 }
 
 export default WaveFactory
