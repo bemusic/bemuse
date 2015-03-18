6e500af2244094fa6506e327fa0f5ba632c7ea4e
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-03-18 16:32:39 +0700

    feat(GameState): judge notes! yay yeah yay yeah!

--- spec/game/state/player-state_spec.js
+++ spec/game/state/player-state_spec.js
@@ -1,18 +1,71 @@
 
-import PlayerState from 'bemuse/game/state/player-state'
+import PlayerState    from 'bemuse/game/state/player-state'
+import Player         from 'bemuse/game/player'
+import GameInput      from 'bemuse/game/input'
+import { notechart }  from '../spec_helper'
 
 describe('PlayerState', function() {
 
   it('updates the input', function() {
     let state = new PlayerState({
-      number:  1,
+      number: 1,
       columns: ['wow'],
+      notechart: notechart(''),
     })
     let input = {
-      get:     name => ({ name })
+      get: name => ({ name })
     }
     state.update(0, input)
     expect(state.input.get('wow').name).to.equal('p1_wow')
+  })
+
+  describe('note judging', function() {
+
+    let chart
+    let player
+    let state
+    let input
+    let buttons
+
+    function setup(bms) {
+      chart   = notechart(bms)
+      player  = new Player(chart, 1, { })
+      state   = new PlayerState(player)
+      input   = new GameInput()
+      buttons = { }
+      input.use({ get: () => buttons })
+    }
+
+    function advance(time, b) {
+      buttons = b
+      input.update()
+      state.update(time, input)
+    }
+
+    it('judges notes', function() {
+
+      setup(`
+        #BPM 120
+        #00111:0101
+      `)
+
+      expect(state.getNoteStatus(chart.notes[0])).to.equal('unjudged')
+
+      advance(1.999, { })
+      expect(state.getNoteStatus(chart.notes[0])).to.equal('unjudged')
+
+      advance(2, { 'p1_1': 1 })
+      expect(state.getNoteStatus(chart.notes[0])).to.equal('judged')
+      expect(state.getNoteJudgment(chart.notes[0])).to.equal(1)
+      expect(state.getNoteStatus(chart.notes[1])).to.equal('unjudged')
+
+      advance(2.1, { 'p1_1': 0 })
+      advance(5,   { 'p1_1': 0 })
+      expect(state.getNoteStatus(chart.notes[1])).to.equal('judged')
+      expect(state.getNoteJudgment(chart.notes[1])).to.equal(-1)
+
+    })
+
   })
 
 })
--- src/game/state/player-state.js
+++ src/game/state/player-state.js
@@ -1,13 +1,71 @@
 
+import R from 'ramda'
+
 export class PlayerState {
   constructor(player) {
-    this._player = player
+    this._player       = player
+    this._notes        = R.sortBy(R.prop('time'), player.notechart.notes)
+    this._noteResult   = new Map()
   }
   update(gameTime, input) {
     let prefix = `p${this._player.number}_`
+    this._gameTime = gameTime
     this.input = new Map(this._player.columns.map((column) =>
         [column, input.get(`${prefix}${column}`)]))
+    this._judgeNotes(gameTime)
   }
+  getNoteStatus(note) {
+    let result = this._noteResult.get(note)
+    if (!result) return 'unjudged'
+    return result.status
+  }
+  getNoteJudgment(note) {
+    let result = this._noteResult.get(note)
+    if (!result) return 0
+    return result.judgment
+  }
+  _judgeNotes() {
+    for (let i = 0; i < this._notes.length; i ++) {
+      let note = this._notes[i]
+      if (this._shouldJudge(note)) {
+        this._judge(note)
+        break
+      }
+    }
+  }
+  _shouldJudge(note) {
+    if (this.getNoteStatus(note) !== 'unjudged') return false
+    let judgment  = judgeTime(this._gameTime, note.time)
+    let missed    = judgment === -1
+    let control   = this.input.get(note.column)
+    let hit       = judgment > 0 && control.changed && control.value
+    return missed || hit
+  }
+  _judge(note) {
+    this._noteResult.set(note, {
+      status: 'judged', judgment: judgeTime(this._gameTime, note.time),
+    })
+  }
+}
+
+/**
+ * Takes a gameTime and noteTime and returns the appropriate judgment.
+ *
+ *  1 - METICULOUS
+ *  2 - PRECISE
+ *  3 - GOOD
+ *  4 - OFFBEAT
+ *  0 - (not judge)
+ * -1 - MISSED
+ */
+export function judgeTime(gameTime, noteTime) {
+  let delta = Math.abs(gameTime - noteTime)
+  if (delta < 0.018) return 1
+  if (delta < 0.040) return 2
+  if (delta < 0.100) return 3
+  if (delta < 0.200) return 4
+  if (gameTime < noteTime) return 0
+  return -1
 }
 
 export default PlayerState
