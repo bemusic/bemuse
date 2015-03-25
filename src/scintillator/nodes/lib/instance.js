679d738397d8a905c76c8f60316bb3db82ca44b6
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-03-25 16:21:16 +0700

    refactor(scintillator/Instance): remove old Instance class
    Since now we have new shiny instance class

--- src/scintillator/nodes/lib/instance.js
+++ src/scintillator/nodes/lib/instance.js
@@ -1,7 +1,7 @@
 
 import Callbacks from 'bemuse/callbacks'
 
-export class Instance2 {
+export class Instance {
   constructor(options) {
     this._context   = options.context
     this._object    = options.object
@@ -84,59 +84,6 @@ export class Instance2 {
   get parent() {
     return this._parent
   }
-}
-
-export function Instance(context, callback) {
-
-  if (arguments.length === 1) {
-    return new Instance2(arguments[0])
-  }
-
-  let destroyCallback = []
-  let dataCallback = []
-
-  let helper = {
-    child(childNode, ...args) {
-      let instance = childNode.instantiate(context, ...args)
-      destroyCallback.push(() => { instance.destroy() })
-      dataCallback.push((data) => { instance.push(data) })
-    },
-    children(array, ...args) {
-      for (let childNode of array) {
-        helper.child(childNode, ...args)
-      }
-    },
-    bind(...pipeline) {
-      let sideEffect = onChange(pipeline.pop())
-      helper.onData(function applyBinding(value) {
-        // using var here to optimize
-        for (var i = 0; i < pipeline.length; i ++) value = pipeline[i](value)
-        sideEffect(value)
-      })
-    },
-    onData(f) {
-      dataCallback.push(f)
-    },
-    onDestroy(f) {
-      destroyCallback.push(f)
-    },
-  }
-
-  callback(helper)
-  callback = null
-
-  return {
-    destroy() {
-      for (let f of destroyCallback) f()
-      destroyCallback = null
-      dataCallback = null
-    },
-    push(data) {
-      if (dataCallback.length === 0) return
-      for (let f of dataCallback) f(data)
-    },
-  }
-
 }
 
 function onChange(f) {
