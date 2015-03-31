6d69bbe51622630620accba47b5f4dce87c1d3df
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-03-31 14:26:47 +0700

    chore(docs): generate module documentation from built source files

--- docs/_extensions/bemuse.py
+++ docs/_extensions/bemuse.py
@@ -1,10 +1,12 @@
 
 from docutils import nodes
+from os import system
 
 def setup(app):
     app.add_role('github', autolink('https://github.com/%s'))
     app.add_role('module', autolink('https://github.com/bemusic/bemuse/tree/master/src/%s'))
     app.add_role('tree', autolink('https://github.com/bemusic/bemuse/tree/master/%s'))
+    app.connect('builder-inited', autogen)
 
 def autolink(pattern):
     def role(name, rawtext, text, lineno, inliner, options={}, content=[]):
@@ -13,3 +15,6 @@ def autolink(pattern):
         return [node], []
     return role
 
+def autogen(app):
+    system('rm -rf modules')
+    system('python generate.py')
