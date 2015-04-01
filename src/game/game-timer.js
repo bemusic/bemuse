2931e5e2547385f5532771ddd35d216aaeac6d2d
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2015-04-01 17:14:38 +0700

    feat(Game): allow pressing enter to start the game

--- src/game/game-timer.js
+++ src/game/game-timer.js
@@ -3,19 +3,42 @@
 // This class should be tied to the AudioContext.
 //
 export class GameTimer {
-  constructor(clock) {
+  constructor(clock, input) {
     this._clock = clock
-    this._started = null
+    this._input = input
+    this.startTime = null
   }
 
-  // The time, in seconds, since the start of the game.
-  get time() {
-    if (this._started === null) {
-      return 0
-    } else {
-      return this._clock.time - this._started
+  get started() {
+    return this.startTime !== null
+  }
+
+  // Updates the timer. This method should be called once in the game loop.
+  update() {
+    this._checkStartGame()
+    // The time, in seconds, since the start of the game.
+    this.time = this._calculateTime()
+  }
+
+  _checkStartGame() {
+    if (!this.started && this._input.get('start').value) {
+      this.startTime = this._clock.time
     }
   }
+
+  _calculateTime() {
+    if (this.startTime === null) {
+      return -0.5
+    } else {
+      var delta = this._clock.time - this.startTime
+      if (delta < 1) {
+        return 0.5 * delta * delta - 0.5
+      } else {
+        return delta - 1
+      }
+    }
+  }
+
 }
 
 export default GameTimer
