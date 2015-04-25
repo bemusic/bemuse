196407013b8077ab8d3db721796dbdfd13eaf498
Thai Pangsakulyanont <dtinth@spacet.me>
2015-04-25 11:53:17 +0700

    chore(tasks/lint): make gulp error when ESLint fail.

--- tasks/lint.js
+++ tasks/lint.js
@@ -1,11 +1,34 @@
 
 import gulp             from 'gulp'
+import gutil            from 'gulp-util'
 import { javascripts }  from '../config/sources'
 import eslint           from 'gulp-eslint'
+import eslintUtil       from 'gulp-eslint/util'
+import through2         from 'through2'
 
 gulp.task('lint', function() {
   return gulp.src(javascripts)
     .pipe(eslint())
     .pipe(eslint.format())
+    .pipe(eslintFailOnError())
 })
+
+function eslintFailOnError() {
+  let error = false
+	return through2.obj(function(file, enc, callback) {
+    if (file.eslint && file.eslint.messages &&
+        file.eslint.messages.some(eslintUtil.isErrorMessage)) {
+      error = new gutil.PluginError('tasks/lint', {
+          name: 'ESLintError',
+          message: 'ESLint Complained!!'})
+    }
+    callback(null, file)
+  }, function(callback) {
+    if (error) {
+      callback(error)
+    } else {
+      callback()
+    }
+	})
+}
 
