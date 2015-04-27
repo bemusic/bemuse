3e2ea1e61ccbc99ccf3e2aeffc2afbbea2c74644
Thai Pangsakulyanont <dtinth@spacet.me>
2015-04-27 22:13:26 +0700

    fix(Analytics): fix ga

--- src/app/analytics.js
+++ src/app/analytics.js
@@ -6,12 +6,12 @@ function getLabel(chart) {
 }
 
 export function gameStart(song, chart) {
-  ga('send', 'song', 'play', song.title)
-  ga('send', 'game', 'start', getLabel(chart), chart.info.level)
+  ga('send', 'event', 'song', 'play', song.title)
+  ga('send', 'event', 'game', 'start', getLabel(chart), chart.info.level)
 }
 
 export function gameFinish(song, chart, gameState) {
   let state = gameState.player(gameState.game.players[0])
-  ga('send', 'song', 'finish', song.title)
-  ga('send', 'game', 'finish', getLabel(chart), state.stats.score)
+  ga('send', 'event', 'song', 'finish', song.title)
+  ga('send', 'event', 'game', 'finish', getLabel(chart), state.stats.score)
 }
