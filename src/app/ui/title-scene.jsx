dd23d39b2885e72d82bdae6179a055e02d7be21c
Thai Pangsakulyanont <dtinth@spacet.me>
2015-05-18 23:32:18 +0700

    feat(TitleScene): update links

--- src/app/ui/title-scene.jsx
+++ src/app/ui/title-scene.jsx
@@ -21,12 +21,13 @@ export default React.createClass({
       </div>
       <SceneToolbar>
         <a onClick={this.showAbout} href="javascript://">About</a>
+        <a onClick={this.openLink} href="https://bemuse.readthedocs.org">Docs</a>
         <span><strong>Bemuse</strong> v{version}</span>
         <SceneToolbar.Spacer />
         <a onClick={this.openLink} href="https://www.facebook.com/bemusegame">Facebook</a>
         <a onClick={this.openLink} href="https://twitter.com/bemusegame">Twitter</a>
+        <a onClick={this.openLink} href="https://medium.com/bemuse-blog">Blog</a>
         <a onClick={this.openLink} href="https://github.com/bemusic/bemuse">GitHub</a>
-        <a onClick={this.openLink} href="https://www.reddit.com/r/bemuse">Community</a>
         <a onClick={this.openLink} href="https://gitter.im/bemusic/bemuse">Chat</a>
       </SceneToolbar>
     </Scene>
