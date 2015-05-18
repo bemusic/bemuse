a6b36829f28d8fb81dfc5fa1f03c2a2fb7e407bd
Thai Pangsakulyanont <dtinth@spacet.me>
2015-05-18 14:23:08 +0700

    feat(~): extract the indexer from the bemusepack project

---
+++ index.js
@@ -0,0 +1,163 @@
+
+var createHash  = require('crypto').createHash
+var bms         = require('bms')
+var Promise     = require('bluebird')
+var _           = require('lodash')
+var lcs         = require('./lcs')
+var assign      = require('object-assign')
+
+var readBMS     = Promise.promisify(bms.Reader.readAsync, bms.Reader)
+
+function getChartInfo(source) {
+  return readBMS(source).then(function(str) {
+    var chart   = bms.Compiler.compile(str).chart
+    var info    = bms.SongInfo.fromBMSChart(chart)
+    var notes   = bms.Notes.fromBMSChart(chart)
+    var timing  = bms.Timing.fromBMSChart(chart)
+    var count   = notes.all().filter(noteIsPlayable).length
+    var bpm     = bpmInfo(notes, timing)
+    var hash    = createHash('md5')
+    hash.update(source)
+    return {
+      md5:        hash.digest('hex'),
+      info:       info,
+      noteCount:  count,
+      scratch:    hasScratch(chart),
+      keys:       detect(chart),
+      bpm:        bpm,
+    }
+  })
+}
+
+exports.getChartInfo = getChartInfo
+
+function getSongInfo(files, options) {
+  var warnings = []
+  var cache = (options && options.cache) || undefined
+  var extra = (options && options.extra) || { }
+  return Promise.map(files, function(file) {
+    var name = file.name
+    var data = file.data
+    var hash = createHash('md5')
+    hash.update(data)
+    var md5 = hash.digest('hex')
+    return Promise.resolve(cache && cache.get(md5))
+    .then(function(cached) {
+      if (cached) {
+        return cached
+      } else {
+        return Promise.resolve(getChartInfo(data)).tap(function(info) {
+          if (cache) return cache.put(md5, info)
+        })
+      }
+    })
+    .then(function(info) {
+      info.file = name
+      return [info]
+    })
+    .catch(function(e) {
+      warnings.push('Unable to parse ' + name + ': ' + e)
+      return []
+    })
+  })
+  .then(_.flatten)
+  .then(function(charts) {
+    if (charts.length === 0) {
+      warnings.push('No usable charts found!')
+    }
+    var song = {
+      title:  common(charts, _.property('info.title')),
+      artist: common(charts, _.property('info.artist')),
+      genre:  common(charts, _.property('info.genre')),
+      bpm:    median(charts, _.property('bpm.median')),
+    }
+    assign(song, extra)
+    song.charts   = charts
+    song.warnings = warnings
+    return song
+  })
+}
+
+exports.getSongInfo = getSongInfo
+
+function noteIsPlayable(note) {
+  return note.column !== undefined
+}
+
+function bpmInfo(notes, timing) {
+  var maxBeat = _(notes.all()).pluck('beat').max()
+  var beats   = _(timing.getEventBeats())
+                  .concat([0, maxBeat])
+                  .sortBy()
+                  .uniq()
+                  .filter(function(beat) { return beat <= maxBeat })
+                  .value()
+  var data    = [ ]
+  for (var i = 0; i + 1 < beats.length; i ++) {
+    var length = timing.beatToSeconds(beats[i + 1]) -
+                    timing.beatToSeconds(beats[i])
+    var bpm    = timing.bpmAtBeat(beats[i])
+    data.push([bpm, length])
+  }
+  var perc = percentile(data)
+  return {
+    init:   timing.bpmAtBeat(0),
+    min:    perc(2),
+    median: perc(50),
+    max:    perc(98),
+  }
+}
+
+function percentile(data) {
+  data = _.sortBy(data, 0)
+  var total = _.sum(data, 1)
+  return function(percentileNo) {
+    var current = 0
+    for (var i = 0; i < data.length; i ++) {
+      current += data[i][1]
+      if (current / total >= percentileNo / 100) return data[i][0]
+    }
+  }
+}
+
+function hasScratch(chart) {
+  var objects = chart.objects.all()
+  for (var object of objects) {
+    var channel = +object.channel
+    if (50 <= channel && channel <= 69) channel -= 20
+    if (channel === 16 || channel === 26) return true
+  }
+  return false
+}
+
+function detect(chart) {
+  var objects = chart.objects.all()
+  var stat = { }
+  for (var object of objects) {
+    var channel = +object.channel
+    if (50 <= channel && channel <= 69) channel -= 40
+    if (channel < 10) continue
+    if (channel > 29) continue
+    stat[channel] = (stat[channel] || 0) + 1
+  }
+  var channels = Object.keys(stat).map(function(ch) { return +ch })
+  if (channels.length === 0) return 'empty'
+  if (channels.some(isSecondPlayer)) {
+    return (stat[18] || stat[19] || stat[28] || stat[29]) ? '14K' : '10K'
+  }
+  return (stat[18] || stat[19]) ? '7K' : '5K'
+
+  function isSecondPlayer(ch) {
+    return 20 <= ch && ch <= 29
+  }
+}
+
+function common(array, f) {
+  var longest = array.map(f).reduce(lcs, [])
+  return String(longest || f(array[0])).trim()
+}
+
+function median(array, f) {
+  var arr = _(array).map(f).sortBy().value()
+  return arr[Math.floor(arr.length / 2)]
+}
---
+++ test.js
@@ -0,0 +1,195 @@
+
+var expect = require('chai').expect
+var indexer = require('./')
+
+require('chai').use(require('chai-as-promised'))
+
+describe('getChartInfo', function() {
+
+  function info(source) {
+    return indexer.getChartInfo(new Buffer(source))
+  }
+
+  describe('.md5', function() {
+    it('should return hash', function() {
+      return expect(info('').get('md5'))
+          .to.eventually.equal('d41d8cd98f00b204e9800998ecf8427e')
+    })
+  })
+
+  describe('.info', function() {
+    it('should return song info', function() {
+      var source = '#TITLE meow'
+      return expect(info(source).get('info').get('title'))
+          .to.eventually.equal('meow')
+    })
+  })
+
+  describe('.noteCount', function() {
+    it('should not count BGM', function() {
+      var source = '#00101:01'
+      return expect(info(source).get('noteCount')).to.eventually.equal(0)
+    })
+    it('should count playable notes', function() {
+      var source = '#00111:01'
+      return expect(info(source).get('noteCount')).to.eventually.equal(1)
+    })
+  })
+
+  describe('.scratch', function() {
+    it('is false when no scratch track', function() {
+      var source = '#00101:01'
+      return expect(info(source).get('scratch')).to.eventually.be.false
+    })
+    it('should count playable notes', function() {
+      var source = '#00116:01'
+      return expect(info(source).get('scratch')).to.eventually.be.true
+    })
+  })
+
+  describe('.keys', function() {
+    it('should be empty when no notes', function() {
+      var source = '#00101:01'
+      return expect(info(source).get('keys')).to.eventually.equal('empty')
+    })
+    it('should be 7K on normal chart', function() {
+      var source = [
+        '#00111:01',
+        '#00114:01',
+        '#00159:0101',
+      ].join('\n')
+      return expect(info(source).get('keys')).to.eventually.equal('7K')
+    })
+    it('should be 5K when 6th and 7th keys not detected', function() {
+      var source = [
+        '#00111:01',
+        '#00114:01',
+      ].join('\n')
+      return expect(info(source).get('keys')).to.eventually.equal('5K')
+    })
+    it('should be 14K on doubles chart', function() {
+      var source = [
+        '#00111:01',
+        '#00114:01',
+        '#00159:0101',
+        '#00121:01',
+        '#00124:01',
+        '#00129:0101',
+      ].join('\n')
+      return expect(info(source).get('keys')).to.eventually.equal('14K')
+    })
+    it('should be 10K when 2 players and ' +
+        '6th and 7th keys not detected', function() {
+      var source = [
+        '#00111:01',
+        '#00114:01',
+        '#00121:01',
+        '#00124:01',
+      ].join('\n')
+      return expect(info(source).get('keys')).to.eventually.equal('10K')
+    })
+  })
+
+  describe('.bpm', function() {
+
+    var source = [
+      '#BPM 120',
+      '#BPM01 240',
+      '#00111:01',
+      '#00108:01',
+      '#00203:3C78',
+      '#00211:01',
+      '#00311:01',
+    ].join('\n')
+
+    var bpm
+
+    beforeEach(function() {
+      return info(source).get('bpm').tap(function(x) { bpm = x })
+    })
+
+    it('init should be the BPM at first beat', function() {
+      expect(bpm.init).to.equal(120)
+    })
+    it('median should be the median BPM', function() {
+      expect(bpm.median).to.equal(120)
+    })
+    it('min should be the minimum BPM', function() {
+      expect(bpm.min).to.equal(60)
+    })
+    it('max should be the maximum BPM', function() {
+      expect(bpm.max).to.equal(240)
+    })
+
+  })
+
+})
+
+
+describe('getSongInfo', function() {
+  
+  describe('with multiple files', function() {
+
+    var files = [
+      {
+        name: '01.bms',
+        data: new Buffer(
+            '#TITLE meow [NORMAL]\n' +
+            '#ARTIST lol\n' +
+            '#PLAYLEVEL 5\n' +
+            '#BPM 123\n' +
+            '#00111:1111'),
+      },
+      {
+        name: '02.bms',
+        data: new Buffer(
+            '#TITLE meow [HYPER]\n' +
+            '#ARTIST lol\n' +
+            '#PLAYLEVEL 7\n' +
+            '#BPM 123\n' +
+            '#00111:1111'),
+      },
+      {
+        name: '03.bms',
+        data: new Buffer(
+            '#TITLE meow [ANOTHER]\n' +
+            '#ARTIST lol\n' +
+            '#PLAYLEVEL 12\n' +
+            '#BPM 123\n' +
+            '#00111:1111'),
+      },
+    ]
+
+    var song
+
+    beforeEach(function() {
+      return indexer.getSongInfo(files).tap(function(x) { song = x })
+    })
+
+    describe('title', function() {
+      it('should be correct', function() {
+        expect(song.title).to.equal('meow')
+      })
+    })
+    describe('artist', function() {
+      it('should be correct', function() {
+        expect(song.artist).to.equal('lol')
+      })
+      it('should be overridable', function() {
+        var options = { extra: { artist: 'meowmeow' } }
+        return expect(indexer.getSongInfo(files, options).get('artist'))
+            .to.eventually.equal('meowmeow')
+      })
+    })
+    describe('charts', function() {
+      it('should be an array of charts', function() {
+        expect(song.charts).to.have.length(3)
+      })
+      it('should have the file key', function() {
+        expect(song.charts[0].file).to.match(/\.bms/)
+      })
+    })
+
+  })
+
+})
