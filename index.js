5e661f21f8f8eca71a1ce24ac4386ab8f8a1742c
Thai Pangsakulyanont <dtinth@spacet.me>
2015-08-03 19:33:37 +0700

    refactor getKeys into its own module

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Promise     = require('bluebird')
 var _           = require('lodash')
 var lcs         = require('./lcs')
 var assign      = require('object-assign')
+var getKeys     = require('./keys')
 
 var readBMS     = Promise.promisify(bms.Reader.readAsync, bms.Reader)
 
@@ -140,29 +141,6 @@ function hasScratch(chart) {
     if (channel === 16 || channel === 26) return true
   }
   return false
-}
-
-function getKeys(chart) {
-  var objects = chart.objects.all()
-  var stat = { }
-  for (var i = 0; i < objects.length; i ++) {
-    var object = objects[i]
-    var channel = +object.channel
-    if (50 <= channel && channel <= 69) channel -= 40
-    if (channel < 10) continue
-    if (channel > 29) continue
-    stat[channel] = (stat[channel] || 0) + 1
-  }
-  var channels = Object.keys(stat).map(function(ch) { return +ch })
-  if (channels.length === 0) return 'empty'
-  if (channels.some(isSecondPlayer)) {
-    return (stat[18] || stat[19] || stat[28] || stat[29]) ? '14K' : '10K'
-  }
-  return (stat[18] || stat[19]) ? '7K' : '5K'
-
-  function isSecondPlayer(ch) {
-    return 20 <= ch && ch <= 29
-  }
 }
 
 function common(array, f) {
