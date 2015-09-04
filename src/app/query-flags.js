6880f32493633d7019631b436407aae0fca542a4
Thai Pangsakulyanont <dtinth@spacet.me>
2015-09-04 18:55:07 +0700

    feat(query-flags): allow `server` query string instead of `BEMUSE_MUSIC_SERVER`

--- src/app/query-flags.js
+++ src/app/query-flags.js
@@ -2,7 +2,7 @@
 import query from 'bemuse/utils/query'
 
 export function getMusicServer() {
-  return query.BEMUSE_MUSIC_SERVER
+  return query.BEMUSE_MUSIC_SERVER || query.server
 }
 
 export function getTimeSynchroServer() {
