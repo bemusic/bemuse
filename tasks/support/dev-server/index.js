509a4ed9e05550106622c4b3d7ad51b7e6c7ae40
Thai Pangsakulyanont <dtinth@spacet.me>
2015-12-04 01:14:21 +0700

    chore(~): replace our testing workflow with Karma

--- tasks/support/dev-server/index.js
+++ tasks/support/dev-server/index.js
@@ -3,46 +3,17 @@ import gutil            from 'gulp-util'
 import webpack          from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
 import express          from 'express'
-import notifier         from 'node-notifier'
 
 import config           from '../../../config/webpack'
 import routes           from '../../../config/routes'
 import path             from '../../../config/path'
 import * as Env         from '../../../config/env'
 
-import * as CoverageReport  from '../coverage-report'
-
-import testMiddleware       from '../test-middleware'
-import testStat             from '../test-middleware/stat'
-
 export function start () {
 
   let port = Env.serverPort()
   var compiler = webpack(config)
   var server = new WebpackDevServer(compiler, config.devServer)
-
-  server.use('/api/test', testMiddleware(function (result) {
-    let { total, failed, passed, _pending } = testStat(result.specs)
-    void _pending
-
-    let notification
-    if (failed > 0) {
-      notification = {
-        title: 'FAILED',
-        message: `${failed}/${total} failed`,
-      }
-    } else {
-      notification = {
-        title: 'PASSED',
-        message: `${passed}/${total} passed`,
-      }
-    }
-    notifier.notify(notification)
-
-    if (result.coverage) {
-      CoverageReport.generate(result.coverage)
-    }
-  }))
 
   for (let route of routes) {
     server.use('/' + route.dest.join('/'), express.static(route.src))
@@ -55,5 +26,4 @@ export function start () {
     if (err) throw new gutil.PluginError('webpack-dev-server', err)
     gutil.log('[webpack-dev-server]', 'http://localhost:' + port + '/')
   })
-
 }
