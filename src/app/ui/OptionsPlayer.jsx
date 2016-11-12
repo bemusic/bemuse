90c71224a9401949aa211a4814db3ac283032856
Thai Pangsakulyanont <dtinth@spacet.me>
2016-11-12 14:20:41 +0700

    refactor(OptionsPlayer): improve cohesiveness of option items

--- src/app/ui/OptionsPlayer.jsx
+++ src/app/ui/OptionsPlayer.jsx
@@ -3,10 +3,8 @@ import * as OptionsIO from '../io/OptionsIO'
 import './OptionsPlayer.scss'
 
 import compose from 'recompose/compose'
-import pure from 'recompose/pure'
 import React from 'react'
 import { connect } from 'react-redux'
-import { withProps } from 'recompose'
 
 import connectIO from '../../impure-react/connectIO'
 import OptionsButton from './OptionsButton'
@@ -15,147 +13,145 @@ import OptionsInputField from './OptionsInputField'
 import OptionsPlayerSelector from './OptionsPlayerSelector'
 import OptionsSpeed from './OptionsSpeed'
 
-const SCRATCH_OPTIONS = [
-  { value: 'left', label: 'Left', },
-  { value: 'right', label: 'Right', },
-  { value: 'off', label: 'Disabled', },
-]
-
-const PANEL_OPTIONS = [
-  { value: 'left', label: 'Left', },
-  { value: 'center', label: 'Center', },
-  { value: 'right', label: 'Right', },
-]
-
-const enhance = compose(
-  connect((state) => ({
-    options: state.options,
-    scratch: Options.scratchPosition(state.options),
-  })),
-  connectIO({
-    onSetPanel: () => (value) => OptionsIO.setOptions({ 'player.P1.panel': value }),
-    onSetScratch: () => (position) => OptionsIO.setScratch(position),
-    onSetSpeed: () => (speed) => OptionsIO.setSpeed(speed),
-    onSetLeadTime: () => (leadTime) => OptionsIO.setLeadTime(leadTime),
-    onSetLaneCover: () => (laneCover) => OptionsIO.setLaneCover(laneCover),
-    onToggleBackgroundAnimationsEnabled: ({ options }) => () => (
-      OptionsIO.setOptions({
-        'system.bga.enabled': Options.toggleOption(options['system.bga.enabled'])
-      })
-    ),
-    onToggleAutoVelocityEnabled: ({ options }) => () => (
-      OptionsIO.setOptions({
-        'player.P1.auto-velocity': Options.toggleOption(options['player.P1.auto-velocity'])
-      })
-    ),
-    onToggleGauge: ({ options }) => () => (
-      OptionsIO.setOptions({
-        'player.P1.gauge': Options.toggleGauge(options['player.P1.gauge'])
-      })
-    )
-  }),
-  pure
-)
+const SettingRow = compose(
+  connect((state) => ({ options: state.options })),
+  connectIO({ onRunIO: () => (io) => io }),
+)((props) => {
+  const { label, isVisible, help, renderControl } = props // user-supplied
+  const { options, onRunIO } = props // from higher-order component
+  const visible = isVisible ? isVisible(options) : true
+  const control = renderControl(options, onRunIO)
+  return (
+    <OptionsPlayer.Row label={label} hidden={!visible}>
+      {control}
+      {!!help && <div className="OptionsPlayerのhelp">{help}</div>}
+    </OptionsPlayer.Row>
+  )
+})
 
 export const OptionsPlayer = React.createClass({
   propTypes: {
-    options: React.PropTypes.object,
-    scratch: React.PropTypes.string,
-    onClose: React.PropTypes.func,
-    onSetPanel: React.PropTypes.func,
-    onSetScratch: React.PropTypes.func,
-    onSetSpeed: React.PropTypes.func,
-    onSetLaneCover: React.PropTypes.func,
-    onToggleBackgroundAnimationsEnabled: React.PropTypes.func,
-    onToggleAutoVelocityEnabled: React.PropTypes.func,
-    onToggleGauge: React.PropTypes.func,
-    onSetLeadTime: React.PropTypes.func
+    onClose: React.PropTypes.func
   },
   render () {
     return <div className="OptionsPlayer">
-      <OptionsPlayer.Row
+      <SettingRow
         label="Speed"
-        hidden={Options.isAutoVelocityEnabled(this.props.options)}
-      >
-        <OptionsSpeed
-          value={this.props.options['player.P1.speed']}
-          onChange={this.props.onSetSpeed}
-        />
-        <div className="OptionsPlayerのhelp">
+        isVisible={(options) => !Options.isAutoVelocityEnabled(options)}
+        renderControl={(options, onRunIO) => (
+          <OptionsSpeed
+            value={options['player.P1.speed']}
+            onChange={(speed) => onRunIO(OptionsIO.setSpeed(speed))}
+          />
+        )}
+        help={<span>
           You can also change the speed in-game<br />using the Up and Down arrow keys.
-        </div>
-      </OptionsPlayer.Row>
+        </span>}
+      />
 
-      <OptionsPlayer.Row
+      <SettingRow
         label="LeadTime"
-        hidden={!Options.isAutoVelocityEnabled(this.props.options)}
-      >
-        <OptionsLeadTimeInputField
-          value={Options.leadTime(this.props.options)}
-          onChange={this.props.onSetLeadTime}
-          style={{ width: '5em' }}
-        />
-        <div className="OptionsPlayerのhelp">
+        isVisible={(options) => Options.isAutoVelocityEnabled(options)}
+        renderControl={(options, onRunIO) => (
+          <OptionsInputField
+            parse={str => parseInt(str, 10)}
+            stringify={value => String(value) + 'ms'}
+            validator={/^\d+(ms)?$/}
+            value={Options.leadTime(options)}
+            onChange={(leadTime) => onRunIO(OptionsIO.setLeadTime(leadTime))}
+            style={{ width: '5em' }}
+          />
+        )}
+        help={<span>
           Speed will be automatically adjusted<br />to maintain a consistent note velocity.
-        </div>
-      </OptionsPlayer.Row>
+        </span>}
+      />
 
-      <OptionsPlayer.Row label="Scratch">
-        <OptionsPlayerSelector type="scratch"
-          options={SCRATCH_OPTIONS}
-          onSelect={this.props.onSetScratch}
-          value={this.props.scratch}
-        />
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="Scratch"
+        renderControl={(options, onRunIO) => (
+          <OptionsPlayerSelector type="scratch"
+            options={[
+              { value: 'left', label: 'Left', },
+              { value: 'right', label: 'Right', },
+              { value: 'off', label: 'Disabled', },
+            ]}
+            onSelect={(position) => onRunIO(OptionsIO.setScratch(position))}
+            value={Options.scratchPosition(options)}
+          />
+        )}
+      />
 
-      <OptionsPlayer.Row label="Panel">
-        <OptionsPlayerSelector type="panel"
-          options={PANEL_OPTIONS}
-          onSelect={this.props.onSetPanel}
-          value={this.props.options['player.P1.panel']}
-        />
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="Panel"
+        renderControl={(options, onRunIO) => (
+          <OptionsPlayerSelector type="panel"
+            options={[
+              { value: 'left', label: 'Left', },
+              { value: 'center', label: 'Center', },
+              { value: 'right', label: 'Right', },
+            ]}
+            onSelect={(value) => onRunIO(OptionsIO.setOptions({ 'player.P1.panel': value }))}
+            value={options['player.P1.panel']}
+          />
+        )}
+      />
 
-      <OptionsPlayer.Row label="Cover">
-        <OptionsLaneCoverInputField
-          value={Options.laneCover(this.props.options)}
-          onChange={this.props.onSetLaneCover}
-          style={{ width: '5em' }}
-        />
-        <div
-          className="OptionsPlayerのhelp"
-          title="Can be negative, in this case the play area is pulled up."
-        >
-          The amount of play area to hide from the top.
-        </div>
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="Cover"
+        renderControl={(options, onRunIO) => (
+          <OptionsInputField
+            parse={str => parseInt(str, 10) / 100}
+            stringify={value => Math.round(value * 100 || 0) + '%'}
+            validator={/^-?\d+(%)?$/}
+            value={Options.laneCover(options)}
+            onChange={(laneCover) => onRunIO(OptionsIO.setLaneCover(laneCover))}
+            style={{ width: '5em' }}
+          />
+        )}
+      />
 
-      <OptionsPlayer.Row label="BGA">
-        <OptionsCheckbox
-          checked={Options.isBackgroundAnimationsEnabled(this.props.options)}
-          onToggle={this.props.onToggleBackgroundAnimationsEnabled}
-        >
-          Enable background animations <span className="OptionsPlayerのhint">(720p, alpha)</span>
-        </OptionsCheckbox>
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="BGA"
+        renderControl={(options, onRunIO) => (
+          <OptionsCheckbox
+            checked={Options.isBackgroundAnimationsEnabled(options)}
+            onToggle={() => onRunIO(OptionsIO.setOptions({
+              'system.bga.enabled': Options.toggleOption(options['system.bga.enabled'])
+            }))}
+          >
+            Enable background animations <span className="OptionsPlayerのhint">(720p, alpha)</span>
+          </OptionsCheckbox>
+        )}
+      />
 
-      <OptionsPlayer.Row label="AutoVel">
-        <OptionsCheckbox
-          checked={Options.isAutoVelocityEnabled(this.props.options)}
-          onToggle={this.props.onToggleAutoVelocityEnabled}
-        >
-          Maintain absolute note velocity <span className="OptionsPlayerのhint">(advanced)</span>
-        </OptionsCheckbox>
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="AutoVel"
+        renderControl={(options, onRunIO) => (
+          <OptionsCheckbox
+            checked={Options.isAutoVelocityEnabled(options)}
+            onToggle={() => onRunIO(OptionsIO.setOptions({
+              'player.P1.auto-velocity': Options.toggleOption(options['player.P1.auto-velocity'])
+            }))}
+          >
+            Maintain absolute note velocity <span className="OptionsPlayerのhint">(advanced)</span>
+          </OptionsCheckbox>
+        )}
+      />
 
-      <OptionsPlayer.Row label="Gauge">
-        <OptionsCheckbox
-          checked={Options.isGaugeEnabled(this.props.options)}
-          onToggle={this.props.onToggleGauge}
-        >
-          Show expert gauge <span className="OptionsPlayerのhint">(experimental)</span>
-        </OptionsCheckbox>
-      </OptionsPlayer.Row>
+      <SettingRow
+        label="Gauge"
+        renderControl={(options, onRunIO) => (
+          <OptionsCheckbox
+            checked={Options.isGaugeEnabled(options)}
+            onToggle={() => onRunIO(OptionsIO.setOptions({
+              'player.P1.gauge': Options.toggleGauge(options['player.P1.gauge'])
+            }))}
+          >
+            Show expert gauge <span className="OptionsPlayerのhint">(experimental)</span>
+          </OptionsCheckbox>
+        )}
+      />
 
       <div className="OptionsPlayerのbuttons">
         <OptionsButton onClick={this.props.onClose}>Save & Exit</OptionsButton>
@@ -176,16 +172,4 @@ OptionsPlayer.Row = React.createClass({
   }
 })
 
-const OptionsLeadTimeInputField = withProps({
-  parse: str => parseInt(str, 10),
-  stringify: value => String(value) + 'ms',
-  validator: /^\d+(ms)?$/
-})(OptionsInputField)
-
-const OptionsLaneCoverInputField = withProps({
-  parse: str => parseInt(str, 10) / 100,
-  stringify: value => Math.round(value * 100 || 0) + '%',
-  validator: /^-?\d+(%)?$/
-})(OptionsInputField)
-
-export default enhance(OptionsPlayer)
+export default OptionsPlayer
