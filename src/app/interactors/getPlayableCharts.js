dcb7e82d3b4c8a65d8275492508d67e1fc30e05a
Thai Pangsakulyanont <dtinth@spacet.me>
2016-11-21 01:09:59 +0700

    :lipstick: Display 5-keys charts first

--- src/app/interactors/getPlayableCharts.js
+++ src/app/interactors/getPlayableCharts.js
@@ -1,11 +1,13 @@
-import isChartPlayable from './isChartPlayable'
 import _ from 'lodash'
+
+import isChartPlayable from './isChartPlayable'
 
 export function getPlayableCharts (charts) {
   return (_(charts)
     .filter(isChartPlayable)
     .orderBy([
       chart => chart.info.difficulty >= 5 ? 1 : 0,
+      chart => chart.keys,
       chart => chart.info.level
     ])
     .value()
