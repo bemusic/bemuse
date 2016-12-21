3bf38be0ad1ece2ece2fdc6f87ab7b7b4e8e4b5d
Thai Pangsakulyanont <dtinth@spacet.me>
2016-12-21 10:19:25 +0700

    :truck: Move official music server out of gh-pages

--- src/music-collection/index.js
+++ src/music-collection/index.js
@@ -1,4 +1,4 @@
-export const OFFICIAL_SERVER_URL = '/music'
+export const OFFICIAL_SERVER_URL = 'https://music.bemuse.ninja/live'
 
 export function load (url) {
   return fetch(url + '/index.json').then(response => response.json())
