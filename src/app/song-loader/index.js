f40937fe78c4a19eee3e29c88a1a76bf4b153d29
Thai Pangsakulyanont <dtinth@spacet.me>
2017-12-24 14:12:37 +0700

    :green_heart: Workaround https://github.com/webpack-contrib/karma-webpack/pull/295 by putting emitted asset in a subdirectory

--- src/app/song-loader/index.js
+++ src/app/song-loader/index.js
@@ -1,4 +1,4 @@
-import Worker from 'worker-loader!./worker.js'
+import Worker from 'worker-loader?name=song-loader/[hash].worker.js!./worker.js'
 
 /* eslint import/no-webpack-loader-syntax: off */
 
