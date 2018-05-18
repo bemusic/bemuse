42a8712c9a76cc1dcb9740c5f0f1dbc13549dece
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-05-18 10:55:33 +0700

    :construction_worker: Setup CircleCI 2

---
+++ dangerfile.ts
@@ -0,0 +1,9 @@
+import { danger, warn } from 'danger'
+import eslint from 'danger-plugin-eslint'
+
+eslint()
+
+// No PR is too small to include a description of why you made a change
+if (danger.github.pr.body.length < 10) {
+  warn('Please include a description of your PR changes.')
+}
