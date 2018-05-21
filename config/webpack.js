336fe87299c81768b8ef89d9de0643d8266b9abe
Resi Respati <resir014@gmail.com>
2018-05-21 12:40:39 +0700

    :bug: remove these browserslists since we use the one on package.json

--- config/webpack.js
+++ config/webpack.js
@@ -107,12 +107,6 @@ function generateLoadersConfig () {
             plugins: () => [
               require('postcss-flexbugs-fixes'),
               require('autoprefixer')({
-                browsers: [
-                  '> 1%',
-                  'last 2 versions',
-                  'Firefox ESR',
-                  'Opera 12.1'
-                ],
                 flexbox: 'no-2009'
               })
             ]
@@ -143,12 +137,6 @@ function generateLoadersConfig () {
             plugins: () => [
               require('postcss-flexbugs-fixes'),
               require('autoprefixer')({
-                browsers: [
-                  '> 1%',
-                  'last 2 versions',
-                  'Firefox ESR',
-                  'Opera 12.1'
-                ],
                 flexbox: 'no-2009'
               })
             ]
