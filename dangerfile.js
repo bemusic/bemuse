eecc41bf49ccb9e0024d074f4dbc7465e6c6b33b
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2018-12-09 02:36:56 +0700

    :wrench: Update Dangerfile to check prettier

--- dangerfile.js
+++ dangerfile.js
@@ -1,19 +1,21 @@
-const { danger, warn, fail } = require('danger')
+const { danger, warn, fail, message } = require('danger')
 const { CLIEngine } = require('eslint')
 const fs = require('fs')
 const toc = require('markdown-toc')
 const prettier = require('prettier')
 
 // No PR is too small to include a description of why you made a change
-if (danger.github.pr.body.length < 10) {
-  warn('Please include a description of your PR changes.')
+if (danger.github) {
+  if (danger.github.pr.body.length < 10) {
+    warn('Please include a description of your PR changes.')
+  }
 }
+
+const filesToCheck = danger.git.created_files.concat(danger.git.modified_files)
 
 // ESLint
 const cli = new CLIEngine({})
-const filesToLint = danger.git.created_files
-  .concat(danger.git.modified_files)
-  .filter(path => !cli.isPathIgnored(path))
+const filesToLint = filesToCheck.filter(path => !cli.isPathIgnored(path))
 const report = cli.executeOnFiles(filesToLint)
 report.results.forEach(result => {
   const { filePath } = result
@@ -28,6 +30,25 @@ report.results.forEach(result => {
     }
   })
 })
+
+// Prettier
+let prettierFailed = false
+filesToCheck.forEach(filePath => {
+  const fileInfo = prettier.getFileInfo.sync(filePath)
+  if (!fileInfo.ignored && fileInfo.inferredParser) {
+    const source = fs.readFileSync(filePath, 'utf8')
+    const options = { parser: fileInfo.inferredParser }
+    if (!prettier.check(source, options)) {
+      fail(`${filePath} is not formatted using Prettier`)
+      prettierFailed = true
+    }
+  }
+})
+if (prettierFailed) {
+  message(
+    'You can run `yarn style:fix` to automatically format all files using Prettier.'
+  )
+}
 
 // Readme
 const readme = fs.readFileSync('README.md', 'utf8')
