8e8b24e5c676d673daa30aa6c95b399eaf337e97
Maurits Lourens <maurits.lourens@gmail.com>
2019-10-02 23:52:32 +0200

    Updates usage of `co` to async function in bemuse-packer.js

--- packages/bemuse-tools/src/bemuse-packer.js
+++ packages/bemuse-tools/src/bemuse-packer.js
@@ -1,5 +1,4 @@
 import Promise from 'bluebird'
-import co from 'co'
 import path from 'path'
 import fs from 'fs'
 import Payload from './payload'
@@ -27,37 +26,32 @@ export class BemusePacker {
     this._refs.push(ref)
     return ref
   }
-  // TODO [#622]: Convert the `write` method to async function (instead of using `co`) in [bemuse-tools] src/bemuse-packer.js
-  // See issue #575 for more details.
-  write(folder) {
-    return co(
-      function*() {
-        let files = []
-        let refs = []
-        let nums = {}
-        for (let ref of this._refs) {
-          let payload = new Payload()
-          for (let file of ref.files) {
-            let [start, end] = payload.add(file.buffer)
-            files.push({ name: file.name, ref: [ref.index, start, end] })
-          }
-          let hash = payload.hash
-          let num = (nums[ref.name] || 0) + 1
-          nums[ref.name] = num
-          let out = ref.name + '.' + num + '.' + hash.substr(0, 8) + '.bemuse'
-          refs.push({ path: out, hash: hash })
-          yield this._writeBin(path.join(folder, out), Buffer.alloc(0), payload)
-          console.log(`Written ${out}`)
-        }
-        let metadata = { files, refs }
-        yield writeFile(
-          path.join(folder, 'metadata.json'),
-          JSON.stringify(metadata)
-        )
-        console.log(`Written metadata.json`)
-      }.bind(this)
+  async write(folder) {
+    let files = []
+    let refs = []
+    let nums = {}
+    for (let ref of this._refs) {
+      let payload = new Payload()
+      for (let file of ref.files) {
+        let [start, end] = payload.add(file.buffer)
+        files.push({ name: file.name, ref: [ref.index, start, end] })
+      }
+      let hash = payload.hash
+      let num = (nums[ref.name] || 0) + 1
+      nums[ref.name] = num
+      let out = ref.name + '.' + num + '.' + hash.substr(0, 8) + '.bemuse'
+      refs.push({ path: out, hash: hash })
+      await this._writeBin(path.join(folder, out), Buffer.alloc(0), payload)
+      console.log(`Written ${out}`)
+    }
+    let metadata = { files, refs }
+    await writeFile(
+      path.join(folder, 'metadata.json'),
+      JSON.stringify(metadata)
     )
+    console.log(`Written metadata.json`)
   }
+
   _writeBin(path, metadataBuffer, payload) {
     let file = fs.createWriteStream(path)
     let size = Buffer.alloc(4)
