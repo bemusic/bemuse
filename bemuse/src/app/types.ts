38e7d0823dfaefbb97cf5e34d8184fb040a76793
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-08 23:43:31 +0700

    Move Song and Chart to collection-model

--- bemuse/src/app/types.ts
+++ bemuse/src/app/types.ts
@@ -1,38 +1,6 @@
 /* eslint camelcase: off */
-/* REASON: These snake case names are used in our JSON files. */
 
-import { IResources } from 'bemuse/resources/types'
 import { PlayerOptionsPlacement } from 'bemuse/game/player'
-
-export type Song = {
-  id: string
-  resources?: IResources
-  path: string
-  tutorial?: boolean
-  video_url?: string
-  video_file?: string
-  video_offset?: number
-  replaygain?: string
-}
-
-export type Chart = {
-  file: string
-  bpm: {
-    init: number
-    min: number
-    max: number
-    median: number
-  }
-  info: {
-    title: string
-    artist: string
-    genre: string
-    subtitles: string[]
-    subartists: string[]
-    difficulty: number
-    level: number
-  }
-}
 
 export type StoredOptions = {
   'system.offset.audio-input': string
---
+++ bemuse/src/collection-model/types.ts
@@ -0,0 +1,36 @@
+/* eslint camelcase: off */
+/* REASON: These snake case names are used in our JSON files. */
+
+import { IResources } from 'bemuse/resources/types'
+
+export type Song = {
+  id: string
+  path: string
+  tutorial?: boolean
+  video_url?: string
+  video_file?: string
+  video_offset?: number
+  replaygain?: string
+
+  /** Added by Bemuse at runtime */
+  resources?: IResources
+}
+
+export type Chart = {
+  file: string
+  bpm: {
+    init: number
+    min: number
+    max: number
+    median: number
+  }
+  info: {
+    title: string
+    artist: string
+    genre: string
+    subtitles: string[]
+    subartists: string[]
+    difficulty: number
+    level: number
+  }
+}
