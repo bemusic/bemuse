9d07ea87fce2dc243bb6e4854fb59eafac72250a
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-17 11:48:03 +0700

    Introduce bemuse-types

--- bemuse/src/collection-model/types.ts
+++ bemuse/src/collection-model/types.ts
@@ -2,38 +2,14 @@
 /* REASON: These snake case names are used in our JSON files. */
 
 import { IResources } from 'bemuse/resources/types'
+import type { Chart, SongMetadataInCollection } from 'bemuse-types'
 
-export type Song = {
-  id: string
-  title: string
-  path: string
-  tutorial?: boolean
-  video_url?: string
-  video_file?: string
-  video_offset?: number
-  replaygain?: string
-  charts: Chart[]
-
-  /** Added by Bemuse at runtime */
+export interface Song extends SongMetadataInCollection {
+  /** Resources that loaded the song file. Added by Bemuse at runtime. */
   resources?: IResources
+
+  /** `true` if this is loaded from a custom song. */
   custom?: boolean
 }
 
-export type Chart = {
-  file: string
-  bpm: {
-    init: number
-    min: number
-    max: number
-    median: number
-  }
-  info: {
-    title: string
-    artist: string
-    genre: string
-    subtitles: string[]
-    subartists: string[]
-    difficulty: number
-    level: number
-  }
-}
+export type { Chart }
