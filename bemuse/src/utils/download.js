72e24edbfb3da8d0fa097a181d7fc4d8fef2b2f7
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-20 21:34:22 +0700

    Retry downloads

--- bemuse/src/utils/download.js
+++ bemuse/src/utils/download.js
@@ -10,25 +10,35 @@ import { BYTES_FORMATTER } from 'bemuse/progress/formatters'
 //    "arraybuffer" or "blob".
 export function download(url) {
   return {
-    as(type, progress) {
-      return new Promise((resolve, reject) => {
-        var xh = new XMLHttpRequest()
-        xh.open('GET', url, true)
-        xh.responseType = type
-        xh.onload = () => {
-          if (+xh.status === 200) {
-            resolve(xh.response)
-          } else {
-            reject(new Error(`Unable to download ${url}: HTTP ${xh.status}`))
+    async as(type, progress) {
+      for (let i = 1; ; i++) {
+        try {
+          return await attempt()
+        } catch (error) {
+          console.error(`Unable to download ${url} [attempt ${i}]`, error)
+          if (i >= 3) throw error
+        }
+      }
+      function attempt() {
+        return new Promise((resolve, reject) => {
+          var xh = new XMLHttpRequest()
+          xh.open('GET', url, true)
+          xh.responseType = type
+          xh.onload = () => {
+            if (+xh.status === 200) {
+              resolve(xh.response)
+            } else {
+              reject(new Error(`Unable to download ${url}: HTTP ${xh.status}`))
+            }
           }
-        }
-        xh.onerror = () => reject(new Error(`Unable to download ${url}`))
-        if (progress) {
-          progress.formatter = BYTES_FORMATTER
-          xh.onprogress = (e) => progress.report(e.loaded, e.total)
-        }
-        xh.send(null)
-      })
+          xh.onerror = () => reject(new Error(`Unable to download ${url}`))
+          if (progress) {
+            progress.formatter = BYTES_FORMATTER
+            xh.onprogress = (e) => progress.report(e.loaded, e.total)
+          }
+          xh.send(null)
+        })
+      }
     },
   }
 }
