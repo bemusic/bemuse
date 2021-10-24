2e933249bd6f35aca49db76061d7980dfa41a696
dtinth on MBP M1 <dtinth@spacet.me>
2021-10-24 17:24:09 +0700

    Allow trailing /index.json when specifying server

--- bemuse/src/music-collection/index.js
+++ bemuse/src/music-collection/index.js
@@ -1,6 +1,10 @@
 export const OFFICIAL_SERVER_URL = 'https://music.bemuse.ninja/live'
 
-export function load(url, { fetch = global.fetch } = {}) {
-  const indexUrl = url.replace(/\/$/, '') + '/index.json'
+export function load(serverUrl, { fetch = global.fetch } = {}) {
+  const indexUrl = getServerIndexFileUrl(serverUrl)
   return fetch(indexUrl).then((response) => response.json())
+}
+
+export function getServerIndexFileUrl(serverUrl) {
+  return serverUrl.replace(/\/(?:index\.json)?$/, '') + '/index.json'
 }
