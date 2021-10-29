334cae300de3973fe3a275f61d8034a850075418
Thai Pangsakulyanont <dtinth@spacet.me>
2021-10-29 16:03:48 +0000

    Fix native promise not having Bluebird methods

--- bemuse/src/bootstrap/index.js
+++ bemuse/src/bootstrap/index.js
@@ -1,33 +1,9 @@
 /* eslint-disable no-extend-native */
-
-// :doc:
-// Bootstraps the environment with:
-//
-// - `debug`_
-// - `Bluebird`_ (with `extended Promise API`_)
-//
-// .. _debug: https://www.npmjs.com/package/debug
-// .. _bluebird: https://github.com/petkaantonov/bluebird
-// .. _Extended Promise API: https://github.com/petkaantonov/bluebird/blob/master/API.md
-
 import 'whatwg-fetch'
+import 'bemuse/promise'
 import debug from 'debug'
-import Bluebird from 'bluebird'
 
-// .. js:data:: DEBUG
-//
-//    An instance of ``debug``. Used for debugging.
+/**
+ * An instance of `debug`. Used for debugging.
+ */
 global.DEBUG = debug
-
-// .. js:data:: Promise
-//
-//    The Global Promise is replaced with Bluebird's implementation.
-global.Promise = Bluebird
-
-// .. js:function:: Promise.prototype.log
-//
-//    The Promise class is augmented with a ``log`` method that simply
-//    logs the value of the promise.
-Promise.prototype.log = function (...args) {
-  return this.tap((value) => console.log(...args.concat([value])))
-}
