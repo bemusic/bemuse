d87c01acc609abb807ba922c458519d420eaa2eb
Thai Pangsakulyanont <dtinth@spacet.me>
2021-12-30 15:04:42 +0700

    Document R key for reloading

--- bemuse/src/previewer/PreviewInfo.tsx
+++ bemuse/src/previewer/PreviewInfo.tsx
@@ -8,7 +8,7 @@ export const PreviewInfo = () => {
       <p>Drop a folder with BMS/bmson files into this window to preview it.</p>
       <p className='PreviewInfoのkeyHints'>
         <kbd>Space</kbd> Play/Pause &middot; <kbd>Left/Right</kbd> Seek &middot;{' '}
-        <kbd>Up/Down</kbd> Hi-Speed &middot;{' '}
+        <kbd>Up/Down</kbd> Hi-Speed &middot; <kbd>R</kbd> Reload
       </p>
     </div>
   )
