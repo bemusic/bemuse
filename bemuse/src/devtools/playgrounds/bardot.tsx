0900aac4029d44d789fc23e904097468904a83e4
dtinth on MBP M1 <dtinth@spacet.me>
2021-12-31 23:05:36 +0700

    Update BarDot playground

--- bemuse/src/devtools/playgrounds/bardot.tsx
+++ bemuse/src/devtools/playgrounds/bardot.tsx
@@ -1,59 +1,19 @@
 import React from 'react'
 import MAIN from 'bemuse/utils/main-element'
 import ReactDOM from 'react-dom'
+import { BarDot } from 'bemuse/previewer/BarDot'
 
 export function main() {
-  ReactDOM.render(<Circ />, MAIN)
-}
-
-const Circ = () => (
-  <svg width={32} height={32} viewBox='0 0 32 32'>
-    <path fill='red' d={describeArc(16, 16, 16, 0, 240)} />
-  </svg>
-)
-
-function polarToCartesian(
-  centerX: number,
-  centerY: number,
-  radius: number,
-  angleInDegrees: number
-) {
-  var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0
-
-  return {
-    x: centerX + radius * Math.cos(angleInRadians),
-    y: centerY + radius * Math.sin(angleInRadians),
-  }
-}
-
-function describeArc(
-  x: number,
-  y: number,
-  radius: number,
-  startAngle: number,
-  endAngle: number
-) {
-  var start = polarToCartesian(x, y, radius, endAngle)
-  var end = polarToCartesian(x, y, radius, startAngle)
-
-  var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1'
-
-  var d = [
-    'M',
-    start.x,
-    start.y,
-    'A',
-    radius,
-    radius,
-    0,
-    largeArcFlag,
-    0,
-    end.x,
-    end.y,
-    'L',
-    x,
-    y,
-  ].join(' ')
-
-  return d
+  ReactDOM.render(
+    <>
+      <BarDot fill='white' fraction={0} />
+      <BarDot fill='white' fraction={0.25} />
+      <BarDot fill='white' fraction={0.5} />
+      <BarDot fill='white' fraction={0.67} />
+      <BarDot fill='white' fraction={0.75} />
+      <BarDot fill='white' fraction={0.8} />
+      <BarDot fill='white' fraction={1} />
+    </>,
+    MAIN
+  )
 }
