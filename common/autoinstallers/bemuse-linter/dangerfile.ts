8699de28625eeb25dcc835721e31d55dfcbb1113
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-17 13:43:09 +0000

    Fix dangerfile crash from a fork

--- common/autoinstallers/bemuse-linter/dangerfile.ts
+++ common/autoinstallers/bemuse-linter/dangerfile.ts
@@ -15,10 +15,8 @@ declare global {
 /* REASON: not compatible with import = require() syntax. */
 
 // No PR is too small to include a description of why you made a change
-if (danger.github) {
-  if (danger.github.pr.body.length < 10) {
-    warn('Please include a description of your PR changes.')
-  }
+if (danger?.github?.pr?.body?.length < 10) {
+  warn('Please include a description of your PR changes.')
 }
 
 const cli = new CLIEngine({} as any)
