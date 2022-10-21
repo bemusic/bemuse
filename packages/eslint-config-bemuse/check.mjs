495823dbe8576ffda5fcde00d73b6cd8d9d722f5
dtinth on MBP M1 <dtinth@spacet.me>
2022-10-22 03:37:28 +0700

    Add more deps

--- packages/eslint-config-bemuse/check.mjs
+++ packages/eslint-config-bemuse/check.mjs
@@ -5,9 +5,22 @@ const rushJson = JSON.parse(
   stripJsonComments(fs.readFileSync('rush.json', 'utf8'))
 )
 
-const { peerDependencies } = JSON.parse(
+const ourPackageJson = JSON.parse(
   fs.readFileSync('packages/eslint-config-bemuse/package.json', 'utf8')
 )
+const expectedPeerDependencies = {
+  ...ourPackageJson.peerDependencies,
+  'eslint-config-bemuse': '^' + ourPackageJson.version,
+}
+
+const eslintrcJs = `// This is a workaround for https://github.com/eslint/eslint/issues/3458
+require('eslint-config-bemuse/patch/modern-module-resolution')
+
+module.exports = {
+  extends: ['bemuse', './.eslintrc.config.import.js'],
+  parserOptions: { tsconfigRootDir: __dirname },
+}
+`
 
 const warnings = []
 
@@ -16,12 +29,29 @@ for (const project of rushJson.projects) {
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'))
 
   // Verify that the project has the correct dependencies.
-  for (const [name, version] of Object.entries(peerDependencies)) {
+  for (const [name, version] of Object.entries(expectedPeerDependencies)) {
+    if (name === packageJson.name) {
+      continue
+    }
     if (packageJson.devDependencies?.[name] !== version) {
       warnings.push(
         `${packageJsonPath}: Missing dev dependency ${name}@${version}`
       )
     }
+  }
+
+  // Verify that the project has a "lint" script.
+  const expectedLintScript = 'eslint --ext .js,.jsx,.ts,.tsx .'
+  if (packageJson.scripts?.lint !== expectedLintScript) {
+    warnings.push(
+      `${packageJsonPath}: Missing "lint" script (expected "${expectedLintScript}")`
+    )
+  }
+
+  // Example
+  const eslintrcJsPath = `${project.projectFolder}/.eslintrc.js`
+  if (!fs.existsSync(eslintrcJsPath)) {
+    fs.writeFileSync(eslintrcJsPath, eslintrcJs)
   }
 }
 
