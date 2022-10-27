ef8051f8cb16abd2d328e2157b7277e221af71d1
MikuroXina <ryosukadnak@gmail.com>
2022-10-27 09:33:40 +0900

    Update condition whether boot script is inlined

--- build-scripts/index.js
+++ build-scripts/index.js
@@ -61,7 +61,9 @@ yargs
   .command('pre-deploy', 'Performs a pre-deploy check', {}, async () => {
     const data = fs.readFileSync('dist/index.html', 'utf-8')
     check('New Relic inlined', () => /NREUM/.test(data))
-    check('Boot script inlined', () => /webpackJsonp/.test(data))
+    check('Boot script inlined', () =>
+      data.includes('__webpack_require__("./boot/index.js")')
+    )
     check('Google Analytics inlined', () => /GoogleAnalyticsObject/.test(data))
 
     function check(title, condition) {
