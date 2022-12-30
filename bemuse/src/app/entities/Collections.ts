fc00795f55cb6487f6aa6a5c20bc12086af21c66
MikuroXina <ryosukadnak@gmail.com>
2022-12-30 15:03:17 +0900

    Add typing for entities

---
+++ bemuse/src/app/entities/Collections.ts
@@ -0,0 +1,31 @@
+import {
+  LoadState,
+  completeWithValue,
+  errorWithReason,
+  initLoading,
+} from './LoadState'
+import produce, { enableMapSet } from 'immer'
+
+enableMapSet()
+
+export type CollectionsState<V> = Map<string, LoadState<V>>
+
+export const initialState = new Map<string, LoadState<unknown>>()
+
+export const beginLoading = <V>(url: string) =>
+  produce((state: CollectionsState<V>) => state.set(url, initLoading()))
+
+export const completeLoading = <V>(url: string, data: V) =>
+  produce((state: CollectionsState<V>) =>
+    state.set(url, completeWithValue(data)())
+  )
+
+export const errorLoading = <V>(url: string, error: Error) =>
+  produce((state: CollectionsState<V>) =>
+    state.set(url, errorWithReason(error)())
+  )
+
+export const getCollectionByUrl =
+  <V>(url: string) =>
+  (state: CollectionsState<V>) =>
+    state.get(url)
