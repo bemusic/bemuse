c7c068dbba23794403e92634a45f0178e77f6586
MikuroXina <ryosukadnak@gmail.com>
2022-12-31 20:33:41 +0900

    Migrate MusicSelectScene

--- bemuse/src/app/io/MusicSearchIO.js
+++ bemuse/src/app/io/MusicSearchIO.js
@@ -3,14 +3,10 @@ import { musicSearchTextSlice } from '../entities/MusicSearchText'
 
 let _timeout
 
-export function handleSearchTextType(text) {
-  return createIO(({ store }) => {
-    store.dispatch(
-      musicSearchTextSlice.actions.MUSIC_SEARCH_TEXT_TYPED({ text })
-    )
-    if (_timeout) clearTimeout(_timeout)
-    _timeout = setTimeout(() => {
-      store.dispatch(musicSearchTextSlice.actions.MUSIC_SEARCH_DEBOUNCED())
-    }, 138)
-  })
+export function handleSearchTextType(text, dispatch) {
+  dispatch(musicSearchTextSlice.actions.MUSIC_SEARCH_TEXT_TYPED({ text }))
+  if (_timeout) clearTimeout(_timeout)
+  _timeout = setTimeout(() => {
+    dispatch(musicSearchTextSlice.actions.MUSIC_SEARCH_DEBOUNCED())
+  }, 138)
 }
